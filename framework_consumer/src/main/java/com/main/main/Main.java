4a180a6a60f1f5f437c32413e4cf189c61ad01ce
chenguanyu <cgychn@163.com>
2021-07-28 13:49:41 +0800

    添加多线程远程调用测试

--- framework_consumer/src/main/java/com/main/main/Main.java
+++ framework_consumer/src/main/java/com/main/main/Main.java
@@ -18,10 +18,33 @@ public class Main {
         TestService testService = MyFrameworkContext.getJustByClass(TestService.class);
 
         String res = testService.test("aaa", 0);
-        System.out.println("quit");
+
+        new Thread(() -> {
+            List<TestTable> l = testService.testList();
+            System.out.println("多线程中输出1--- " + l);
+        }).start();
+
         List<TestTable> lists = testService.testList();
 
+        new Thread(() -> {
+            List<TestTable> l = testService.testList();
+            System.out.println("多线程中输出2--- " + l);
+        }).start();
+
         TestTable testTable = testService.testOne(0);
+
+        new Thread(() -> {
+            TestTable l = testService.testOne(0);
+            System.out.println("多线程中输出3--- " + l);
+        }).start();
+        new Thread(() -> {
+            String l = testService.test("bbbwww", 0);
+            System.out.println("多线程中输出4--- " + l);
+        }).start();
+        new Thread(() -> {
+            List<TestTable> l = testService.testList();
+            System.out.println("多线程中输出5--- " + l);
+        }).start();
 
 //        System.out.println(testService.test("aaa", 123));
 
